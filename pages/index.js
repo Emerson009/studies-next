d213bea940d146ee71a8d788fdbf454ddd0b4877
Emerson009 <henriqueemerson023@gmail.com>
2023-02-06 11:46:54 -0300

    add-estilos

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,35 @@
-import Link from 'next/link'
+import HomeScreen from "../src/screens/HomeScreen";
 
-export default function HomePage(){
-    return (
-        <div>
-            <h1>Sanctum Emovere - Home</h1>
-            <Link href="/faq" passHref>
-                Ir para o FAQ
-            </Link>
-        </div>
-    )
-}
+export default HomeScreen
+// import Link from 'next/link'
+// import React from 'react'
+// import Footer from '../src/components/patterns/Footer';
+
+
+// function Title({children, as}){
+//     const Tag = as;
+//     return(
+//         <React.Fragment>
+//             <Tag>
+//                 {children}
+//             </Tag>
+//             <style jsx>{`
+//                 ${Tag}{
+//                     color: red;
+//                 }
+//             `}</style>
+//         </React.Fragment>
+//     )
+// }
+
+// export default function HomePage(){
+//     return (
+//         <div>
+//             <Title as="h1">Sanctum Emovere - Home</Title>
+//             <Link href="/faq" passHref>
+//                 Ir para o FAQ
+//             </Link>
+//             <Footer/>
+//         </div>
+//     )
+// }
---
+++ src/components/Link/index.js
@@ -0,0 +1,11 @@
+// import Link from 'next/link'
+
+// export default function HomePage({children, href, ...props}){
+//     return(
+//         <div>
+//             <Link href={href} passHref>
+//             <a{...props}>{children}</a>
+//             </Link>
+//         </div>
+//     )
+// }
